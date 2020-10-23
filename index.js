ab16464980d846055d834af09db7f2c78188f93e
bgfxc4 <bindermarc@gmx.de>
2020-10-23 18:57:25 +0200

    local save now working properly

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ window.onload = function load() {
 function add_button_clicked() {
 	console.log("klick");
 	if(current_state == STATE.choose_person)
-		new_person(prompt("Please Enter the name of the Person you want to add."));
+		new_person(prompt("Please enter the name of the person you want to add."));
 	else if(current_state == STATE.choose_quote)
-		new_quote(prompt("Please Enter the Quote you want to add."));
+		new_quote(prompt("Please enter the quote you want to add."));
 }
 
 function create_person_button(name) {
 	var main_div = document.getElementById("main_div");
 	var pers = document.createElement("span");
-	pers.setAttribute("onclick", `load_persons_quotes(\"${name}\")`);
+	pers.setAttribute("onclick", `person_button_clicked(\"${name}\")`);
 	pers.setAttribute("class", "person_button");
 	pers.innerHTML = decodeURIComponent(name);
 	main_div.appendChild(pers);
@@ -56,7 +56,7 @@ function new_person(name) {
 function new_quote(quote) {
 	console.log(quote);
 	if(quote.trimLeft() == "") {
-		alert("The Quote cant be an empty string!");
+		alert("The quote cant be an empty string!");
 		return;
 	}
 	create_quote_button(quote);
@@ -118,16 +118,38 @@ function exit_rem_mode() {
 		console.log("exit rem");
 		all_buttons[i].classList.remove("red_button");
 	}
+	
+	if (current_state == STATE.rem_person) {
+		current_state = STATE.choose_person;
+		hide_return_button();
+	} else if (current_state == STATE.rem_quote) {
+		current_state = STATE.choose_quote;
+	}
+
 }
 
 function rem_button_clicked() {
-	if(current_state == STATE.choose_person) {
+	if (current_state == STATE.choose_person) {
 		current_state = STATE.rem_person;
 		show_return_button();
 		enter_rem_mode();
-	} else if(current_state == STATE.choose_quote) {
+	} else if (current_state == STATE.choose_quote) {
 		current_state = STATE.rem_quote;
 		enter_rem_mode();
+	}
+}
+
+function person_button_clicked(name) {
+	if (current_state == STATE.rem_person) {
+		remove_person(name);	
+	} else if (current_state == STATE.choose_person) {
+		load_persons_quotes(name);
+	}
+}
+
+function quote_button_clicked(index) {
+	if (current_state == STATE.rem_quote) {
+		remove_quote(index);
 	}
 }
 
@@ -137,7 +159,7 @@ function load_persons_quotes(name) {
 	current_loaded_person = name;
 	if(localStorage.getItem(name) == null) localStorage.setItem(name, "");
 	var quotes_splited = localStorage.getItem(name).split("|");
-	all_persons[get_person_by_name(name)].quotes = quotes_splited;
+	all_persons[get_person_index_by_name(name)].quotes = quotes_splited;
 	console.log("loading quotes from: " + name + ", quotes: " + quotes_splited);
 	show_return_button();
 
@@ -145,12 +167,43 @@ function load_persons_quotes(name) {
 	for(var i = 0; i < quotes_splited.length; i++) {
 		var quote = document.createElement("span");
 		quote.setAttribute("class", "person_button");
+		quote.setAttribute("onclick", `quote_button_clicked(${i})`)
 		quote.innerHTML = decodeURIComponent(quotes_splited[i]);
 		main_div.appendChild(quote);
 	}
 }
 
-function get_person_by_name(name) {
+function remove_person(name) {	
+	if(!confirm(`Are you sure that you want to delete ${name}?`)) return;
+	for(var i = 0; i < all_persons.length; i ++) {
+		if(name == all_persons[i].name) {
+			all_persons.splice(i, 1);	
+		}
+	}
+	save_to_localstorage();
+	localStorage.removeItem(encodeURIComponent(name));
+	document.getElementById("main_div").innerHTML = "";
+	exit_rem_mode();
+	load_persons();
+}
+
+function remove_quote(index) {
+	if (!confirm(`Are you sure that you want to delete this quote from ${current_loaded_person}?`)) {
+		return;
+	}
+	console.log(index);
+	var pers_index = get_person_index_by_name(current_loaded_person);
+	all_persons[pers_index].quotes.splice(index, 1);
+	var tosave = "";
+	for (var i = 0; i < all_persons[pers_index].quotes.length; i ++) {
+		tosave += "|" + encodeURIComponent(all_persons[pers_index].quotes[i]);
+	}
+	localStorage.setItem(encodeURIComponent(current_loaded_person), tosave);
+	document.getElementById("main_div").innerHTML = "";
+	load_persons_quotes(current_loaded_person);
+}
+
+function get_person_index_by_name(name) {
 	for(var i = 0; i < all_persons.length; i ++) {
 		if(name == all_persons[i].name) return i;
 	}
